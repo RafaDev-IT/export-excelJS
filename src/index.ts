e5cf417d3e6ed8fe6332d94365e3141ce4d295ca
ASESUISA\rmartinez <rmartinez@asesuisa.com>
2021-07-03 09:46:36 -0600

    ♻️ refactor code

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import ExcelJS from 'exceljs'
+import saveAs from 'file-saver'
+
+// type props<T> = {
+//     titleFields: Array<T>,
+//     data, 
+//     nameDocument
+// }
+/**
+ * 
+ * @param titles
+ * Ejemplo: titles:['n1', 'n2', 'n3']
+ * @param data
+ * Ejemplo: data: [[1,2,3],[4,5,6]]
+ * @param documentName
+ * Ejemplo: documentName: 'prueba'
+ * @returns 
+ */
+const SaveExcel = async ({ titles, data, documentName }) => {
+    try {
+        const wb = new ExcelJS.Workbook();
+        const ws = wb.addWorksheet();
+        const rowHeader = ws.addRow(titles);
+        rowHeader.font = { bold: true }
+
+        data.forEach((item) => ws.addRow(item))
+        const buf = await wb.xlsx.writeBuffer();
+        return saveAs(new Blob([buf]), documentName + '.xlsx');
+    } catch (error) {
+        return(error)
+    }
+}
+
+export default SaveExcel
